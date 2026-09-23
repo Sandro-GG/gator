1e55d56e4c935b8688b0374778116ed827e94026
Sandro Gakharia <sandrogakharia@gmail.com>
2026-09-23 22:28:34 +0400

    feat: add config package with Read and SetUser

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/Sandro-GG/gator/internal/config"
+)
+
 func main() {
+	content, err := config.Read()
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	_, err = content.SetUser("Sandro")
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	content, err = config.Read()
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	fmt.Printf("db_url: %s\ncurrent_user_name: %s", content.DbUrl, content.CurrentUsername)
 
 }
